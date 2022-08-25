992c80bda767be72547d6cb902b9e660f822c35d
wayungi <fwayungi@gmail.com>
2022-08-26 01:04:14 +0300

    Remove unused imports

--- src/components/__tests__/Rocket.test.js
+++ src/components/__tests__/Rocket.test.js
@@ -2,10 +2,7 @@
 
 import { cleanup, render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-// import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
-import ReactDom from 'react-dom';
-// import renderer from 'react-test-renderer';
 import Rocket from '../Rocket';
 import store from '../../redux/configureStore';
 
