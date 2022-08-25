a2e53ccc10978a235e04a8a399831a03ac9e4364
wayungi <fwayungi@gmail.com>
2022-08-25 18:52:52 +0300

    Add proptypes

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,7 +1,22 @@
+/** @format */
+import { useSelector } from 'react-redux';
+import styles from './MyProfile.module.css';
+
 function MyProfile() {
+  const rockets = useSelector((state) => state.rockets);
+  const missions = useSelector((state) => state.missions);
+
   return (
-    <div>
-      <h1>My Profile</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.missions}>
+        <h1>My Missions</h1>
+        <div className={styles.reservedRockets}>reserved rockets</div>
+      </div>
+
+      <div className={styles.rockets}>
+        <h1>My Rockets</h1>
+        <div className={styles.rerservdMissions}>reserevd missions</div>
+      </div>
     </div>
   );
 }
