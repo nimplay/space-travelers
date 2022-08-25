999ba47a8de0539e50bf9d737ba771a12b3974d4
wayungi <fwayungi@gmail.com>
2022-08-25 19:23:01 +0300

    Add borders to reserved rockets in profile page'

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,16 +1,25 @@
 /** @format */
 import { useSelector } from 'react-redux';
 import styles from './MyProfile.module.css';
+import ProfileItem from '../components/ProfileItem';
 
 function MyProfile() {
   const rockets = useSelector((state) => state.rockets);
-  const missions = useSelector((state) => state.missions);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  // const missions = useSelector((state) => state.missions);
+  // const reservedMissions = rockets.filter((mission) => mission.reserved === true );
+
+  console.log(reservedRockets);
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.missions}>
         <h1>My Missions</h1>
-        <div className={styles.reservedRockets}>reserved rockets</div>
+        <div className={styles.reservedRockets}>
+          {reservedRockets.map((reservedRocket) => (
+            <ProfileItem key={reservedRocket.id} item={reservedRocket.rocket_name} />
+          ))}
+        </div>
       </div>
 
       <div className={styles.rockets}>
